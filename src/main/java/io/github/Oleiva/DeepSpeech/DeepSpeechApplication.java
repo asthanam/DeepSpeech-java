242dfd856d046fbdf8ba80dcbdc8d755a4ed5f0b
Oleh <oivashko@zaraffasoft.com>
2018-04-22 00:29:21 +0300

    init repository

---
+++ src/main/java/io/github/Oleiva/DeepSpeech/DeepSpeechApplication.java
@@ -0,0 +1,12 @@
+package io.github.Oleiva.DeepSpeech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSpeechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepSpeechApplication.class, args);
+	}
+}
